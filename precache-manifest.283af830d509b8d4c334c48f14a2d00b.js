5cef707a2f8275569e44a4cf1202140b1b3e0057
中島　亮三 <aki323buri2@gmail.com>
2020-03-24 02:15:55 +0900

    Updates

---
+++ precache-manifest.283af830d509b8d4c334c48f14a2d00b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bb62a65d53b76b1d1515570506cd8c",
+    "url": "/daieii-menu/index.html"
+  },
+  {
+    "revision": "ff3a6952af9c1235f5ea",
+    "url": "/daieii-menu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33790ef6456384d336ef",
+    "url": "/daieii-menu/static/js/2.864b3dcf.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/daieii-menu/static/js/2.864b3dcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3a6952af9c1235f5ea",
+    "url": "/daieii-menu/static/js/main.ae206131.chunk.js"
+  },
+  {
+    "revision": "3f34026664672eb112d9",
+    "url": "/daieii-menu/static/js/runtime-main.6041001c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daieii-menu/static/media/logo.5d5d9eef.svg"
+  }
+]);
